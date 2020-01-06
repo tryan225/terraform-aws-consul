6e30247d2cee2f9921d801431ff80cc35ea72669
tryan225 <tryan225@icloud.com>
2020-01-06 13:14:41 -0500

    Added packer and TF variables for this scenario

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@
 variable "ami_id" {
   description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/consul-ami/consul.json. To keep this example simple, we run the same AMI on both server and client nodes, but in real-world usage, your client nodes would also run your apps. If the default value is used, Terraform will look up the latest AMI build automatically."
   type        = string
-  default     = null
+  default     = "ami-06d5482fb888b5055"
 }
 
 variable "cluster_name" {
